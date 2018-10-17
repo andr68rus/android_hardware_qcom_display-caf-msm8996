712b46a23d5fd30745e27af821b2b6b0c7c1666e
andr68rus <ashtmb@gmail.com>
2018-10-17 08:46:04 +0300

    adjust build for Omni: * Use omni pathmap (by @maxwen) * Don't build lights module if vendor supplies it (by @cyanogen)

--- Android.mk
+++ Android.mk
@@ -1,9 +1,15 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8996)
+
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils libdisplayconfig
+endif
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
 endif
 
 display-hals += gralloc
@@ -14,4 +20,6 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+endif
+
 endif
